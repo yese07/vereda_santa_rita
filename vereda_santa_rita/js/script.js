7f53b270342b08ce52ef1172f7917118e6719ea9
yese07 <78973872+yese07@users.noreply.github.com>
2021-05-20 20:50:25 -0500

    con menu hamburguesa

--- vereda_santa_rita/js/script.js
+++ vereda_santa_rita/js/script.js
@@ -1,1 +1,6 @@
-alert ("hola")
+var btn_menu = document.querySelector('.btn_menu');
+var menu_items = document.getElementById('menu_items');
+
+btn_menu.addEventListener('click', () => {
+            menu_items.classList.toggle('show')
+        })
